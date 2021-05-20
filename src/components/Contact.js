924e39006eb177c76e5eaf73ab62b5d1cf22e0fd
Francisco Fernandez <francisco59553@gmail.com>
2021-05-20 18:20:16 +0200

    feat: adding axios to communicate with apinodemailer

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,43 +1,118 @@
-import { useTheme } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Contact() {
+  const [contactForm, setContactForm] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  function onUpdate(e) {
+    setContactForm({
+      ...contactForm,
+      [e.target.name]: e.target.value,
+    });
+    // console.log(contactForm);
+  }
+
+  function submitEmail(e) {
+    e.preventDefault();
+    axios({
+      method: "POST",
+      url: "http://localhost:8080/send",
+      data: contactForm,
+    }).then((response) => {
+      if (response.data.status === "success") {
+        console.log("ca marche");
+        alert("Message Sent.");
+        resetForm(e);
+      } else if (response.data.status === "fail") {
+        alert("Message failed to send.");
+        console.log("ca marche pas");
+      }
+    });
+  }
+
+  function resetForm(e) {
+    e.target.reset();
+    console.log("reset ", e);
+  }
+
   return (
-    <div
-      style={{
-        flexDirection: "column",
-        width: "100%",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <div>
-        <pre>
-          <h1>/!\ Page en construction /!\</h1>
-        </pre>
-      </div>
-      <div
-        style={{
-          border: `solid 2px ${useTheme().palette.secondary.dark}`,
-          paddingRight: "20px",
-        }}
-      >
-        <ul>
-          <li>Francisco FERNANDEZ</li>
-          <li>francisco59553@gmail.com</li>
-          <li>06.58.70.43.27</li>
-          <br />
-          <a
-            style={{ color: useTheme().palette.text.primary }}
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.linkedin.com/in/francisco-fernandez-57990a104/"
-          >
-            <li>https://www.linkedin.com/in/francisco-fernandez-57990a104/</li>
-          </a>
-        </ul>
+    <div className="section">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="section-title">
+              <h2 className="title">Contact Us</h2>
+              <p>
+                Let us know what you think! In order to provide better service,
+                please do not hesitate to give us your feedback. Thank you.
+              </p>
+              <hr />
+              <form id="contact-form" onSubmit={submitEmail.bind(this)} method="POST">
+                <div className="form-group">
+                  <div className="row">
+                    <div className="col-md-6">
+                      <input
+                        name="name"
+                        placeholder="Name"
+                        id="name"
+                        type="text"
+                        className="form-control"
+                        required
+                        value={contactForm.name}
+                        onChange={onUpdate}
+                      />
+                    </div>
+                    <div className="col-md-6">
+                      <input
+                        name="email"
+                        placeholder="Email"
+                        id="email"
+                        type="email"
+                        className="form-control"
+                        aria-describedby="emailHelp"
+                        required
+                        value={contactForm.email}
+                        onChange={onUpdate}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="form-group">
+                  <input
+                    name="subject"
+                    placeholder="Subject"
+                    id="subject"
+                    type="text"
+                    className="form-control"
+                    required
+                    value={contactForm.subject}
+                    onChange={onUpdate}
+                  />
+                </div>
+                <div className="form-group">
+                  <textarea
+                    name="message"
+                    placeholder="Message"
+                    id="message"
+                    className="form-control"
+                    rows="1"
+                    required
+                    value={contactForm.message}
+                    onChange={onUpdate}
+                  />
+                </div>
+                <button type="submit" className="primary-btn submit">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
